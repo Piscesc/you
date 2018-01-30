91c52f0234ae75f4d1c383239a357dc28956cad0
hunter36 <673389081@qq.com>
2018-01-30 17:50:20 +0800

    merge

--- src/main.js
+++ src/main.js
@@ -4,17 +4,13 @@ import router from './router'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'assets/css/reset.css'
 import 'swiper/dist/css/swiper.css'
-<<<<<<< HEAD
 import Mint from 'mint-ui'
 import 'mint-ui/lib/style.css'
-
-Vue.use(Mint)
-
-=======
 import 'assets/css/iconfont/iconfont.css'
->>>>>>> b28a84ccc9f0a5b85b3baded29d81c3934d85de1
+
 Vue.config.productionTip = false
 Vue.use(VueAwesomeSwiper)
+Vue.use(Mint)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
